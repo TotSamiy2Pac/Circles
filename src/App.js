67c10fe9506d37c54ffb936f9acbe3fac5358613
Erjoh <erjik1902@gmail.com>
2023-09-14 16:54:40 +0600

    eerzhanpushit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import Sign from "./components/sign";
-import Layout from "./components/layout/Layout";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DevelopmentPage from "./pages/DevelopmentPage";
+import ArtPage from "./pages/ArtPage";
+import SportPage from "./pages/SportPage";
+import PosterPage from "./pages/PosterPage";
 
 const App = () => {
   return (
-      <Layout>
-          <Sign />
-      </Layout>
+          <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/choice/:name' element={<DevelopmentPage />}/>
+              <Route path='/choice/:name' element={<SportPage />}/>
+              <Route path='/choice/:name' element={<ArtPage />}/>
+              <Route path='/poster' element={<PosterPage />}/>
+          </Routes>
   );
 };
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from "../components/layout/Layout";
+import ChoiceComponent from "./ChoiceComponent";
 
 const HomePage = () => {
     return (
         <Layout>
             <h3>Home Page</h3>
+            <ChoiceComponent/>
         </Layout>
     );
 };
