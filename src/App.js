d58063e114beb16a97096ae49a6cac0dfc285760
askerbekov <askerbekov.mederbek@gmail.com>
2023-09-14 16:54:29 +0600

    added component get clubs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from "./components/SignIn";
+import ClubsTitle from "./components/clubsTitle";
 
 const App = () => {
   return (
       <div>
           <SignIn />
+          <ClubsTitle/>
       </div>
   );
 };
