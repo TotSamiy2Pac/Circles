299cd99c648b6cc64d6eca3809e52715c662f1d7
Erjoh <erjik1902@gmail.com>
2023-09-17 23:49:09 +0600

    Fixed home page

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -7,9 +7,7 @@ import Button from "../../components/buttons/button";
 const HomePage = () => {
     return (
         <Layout>
-            <р>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, repellat.
-            </р>
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aspernatur, incidunt obcaecati possimus quia tenetur. Doloremque eveniet quasi vel voluptas voluptates. Debitis deleniti ex in quaerat quis! A aspernatur et, eum harum illo maxime nam quo totam voluptas voluptate? Amet blanditiis culpa cum deleniti deserunt dolores doloribus dolorum ex expedita impedit in itaque iure laborum maiores nobis nulla, odio officia perferendis perspiciatis quae quasi quibusdam quo totam veritatis, voluptatibus. Ad animi aperiam, atque aut debitis dolores ea eos error est et eveniet ex fugit harum illum inventore, ipsum laudantium mollitia odio perferendis quidem ratione repellat sequi sit sunt suscipit vel voluptate! At eius molestiae sint. Aut blanditiis consequatur cumque cupiditate excepturi, fuga harum hic impedit magnam maiores molestiae nam odio, quis quisquam rerum tenetur totam veniam voluptas voluptate voluptatem. Adipisci assumenda culpa expedita inventore ipsam, molestiae nemo quibusdam quisquam sequi tempore? Atque deserunt fugit vero? Aut beatae commodi consequuntur cum enim, error iure mollitia optio quo reiciendis reprehenderit rerum soluta, suscipit temporibus totam voluptatem voluptates? Assumenda cupiditate fugit laborum, neque possimus quibusdam! Accusantium, aliquid amet autem cupiditate dolorem, eaque id impedit incidunt ipsa minima mollitia, odit optio quos reiciendis soluta totam ut voluptatem? Ab consectetur corporis dolorem illo nam.</span>
             <h1>Lorem ipsum dolor sit amet.</h1>
             <h3>Home Page</h3>
             <div className={'btn-wrapper'}>
